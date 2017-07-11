847e91b4fad64eb24742ad3b0d148089e67e732d
goreleaserbot <goreleaser@carlosbecker.com>
2017-07-11 19:30:33 +0200

    tpm version v0.3.1

--- Formula/tpm.rb
+++ Formula/tpm.rb
@@ -1,9 +1,9 @@
 class Tpm < Formula
   desc "A CLI client to search and manage passwords inside TeamPasswordManager."
   homepage "https://github.com/martensson/tpm"
-  url "https://github.com/martensson/tpm/releases/download/v0.2.3/tpm_0.2.3_darwin_amd64.tar.gz"
-  version "0.2.3"
-  sha256 "c92e3007eaf3642c02326536554220456757c76189c667c6922afc5f368ec301"
+  url "https://github.com/martensson/tpm/releases/download/v0.3.1/tpm_0.3.1_darwin_amd64.tar.gz"
+  version "0.3.1"
+  sha256 "37fb4b237f51e05bdacf56913eeb6670c2cded7d1ae473375674c3a55144b824"
 
   def install
     bin.install "tpm"
